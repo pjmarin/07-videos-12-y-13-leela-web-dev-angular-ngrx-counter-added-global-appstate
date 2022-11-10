f30cdcbae8014d9c7fd413464a2396e8cd59ca25
pjmarin <39600116+pjmarin@users.noreply.github.com>
2022-11-10 09:13:37 +0100

    Added posts state and global appState

---
+++ src/app/counter/counter-output/counter-output.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable, of, Subscription } from 'rxjs';
+import { getCounter } from '../state/counter.selectors';
+import { CounterState } from '../state/counter.state';
+
+@Component({
+  selector: 'app-counter-output',
+  templateUrl: './counter-output.component.html',
+  styleUrls: ['./counter-output.component.css']
+})
+
+// Opicion 2 con async pipe
+export class CounterOutputComponent implements OnInit {
+  counter$: Observable<number | undefined> = of(0);
+  constructor(private store: Store<{ counter: CounterState }>) { }
+
+  ngOnInit(): void {
+    this.counter$ = this.store.select(getCounter);
+  }
+
+}
---
+++ src/app/counter/custom-counter-input/custom-counter-input.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { changeChannelName, customIncrement } from '../state/counter.actions';
+import { getChannelName } from '../state/counter.selectors';
+import { CounterState } from '../state/counter.state';
+import { Observable, of } from 'rxjs';
+
+@Component({
+  selector: 'app-custom-counter-input',
+  templateUrl: './custom-counter-input.component.html',
+  styleUrls: ['./custom-counter-input.component.css']
+})
+export class CustomCounterInputComponent implements OnInit {
+  value: number = 0;
+  channelName$: Observable<string | undefined> = of("");
+  constructor(private store: Store<{ counter: CounterState }>) { }
+
+  ngOnInit(): void {
+    this.channelName$ = this.store.select(getChannelName);
+  }
+
+  onAdd() {
+    this.store.dispatch(customIncrement({ count: +this.value }));
+  }
+
+  onChangeChannelName() {
+    this.store.dispatch(changeChannelName());
+  }
+
+}
